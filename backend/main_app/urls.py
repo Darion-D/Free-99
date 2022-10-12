4683bc51f4cc680d39c5159be506591f3840a52f
DarionDaire <DarionDaire@gmail.com>
2022-10-12 14:53:56 -0400

    Add styling

--- backend/main_app/urls.py
+++ backend/main_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.Home.as_view(), name='home'),
+    path('about/', views.About.as_view(), name='about'),
 ]
--- backend/main_app/views.py
+++ backend/main_app/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from django.views import View
 from django.http import HttpResponse
 from django.views.generic.base import TemplateView
-import requests
 
 
-    
-def home(request):
-    games=requests.get('https://www.freetogame.com/api/games').json()
-    return render(request, 'home.html', {'games': games})
+class Home(TemplateView):
+    template_name = 'home.html'
 
-    
+class About(TemplateView):
+    template_name = 'about.html'
