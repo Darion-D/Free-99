35ff7a28b0cb141a898ca6ba1c7a9807bda876f8
DarionDaire <DarionDaire@gmail.com>
2022-10-12 10:24:19 -0400

    initial commit

---
+++ backend/main_app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
+
+class Home(View):
+
+    def get(self, request):
+        return HttpResponse('Free99 Home')
