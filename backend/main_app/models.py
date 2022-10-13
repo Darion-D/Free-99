6af179ba74eee73d7a54139d903f6738af2746bb
DarionDaire <DarionDaire@gmail.com>
2022-10-13 11:20:43 -0400

    Add User model

--- backend/main_app/models.py
+++ backend/main_app/models.py
@@ -1,5 +1,6 @@
 from operator import mod
 from django.db import models
+from django.contrib.auth.models import User
 
 class Game(models.Model):
     title = models.CharField(max_length=100)
@@ -22,6 +23,7 @@ class Game(models.Model):
 class FavoritesList(models.Model):
     title = models.CharField(max_length=150)
     games = models.ManyToManyField(Game)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
 
     def __str__(self):
         return self.title
